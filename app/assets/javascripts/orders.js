fc60f4d81d2f846d12cf8b3de3ca5183afebd27f
himesh <himesh@pixelcrayons.com>
2016-03-21 17:08:03 +0530

    added warning message for order's quantity

--- app/assets/javascripts/orders.js
+++ app/assets/javascripts/orders.js
@@ -12,6 +12,12 @@ $(document).ready(function(){
 		var orderAmountContainer = trContainer.find('.order_order_items_amount input');
 		var amount = orderPrice * quantity
 		orderAmountContainer.val(amount)
+
+		var productQuantity = parseInt(trContainer.find("#product_quantity").val());
+		var spanContainer = trContainer.find('td span');
+		if(productQuantity < quantity){
+			spanContainer.html("<p style='color:red'>You are trying to order in excess to what we have in the inventory</p>")
+		}
 	});
 
 	$("#order_customer_id").change(function(){
