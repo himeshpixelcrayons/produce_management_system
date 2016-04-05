8bc6f8a79df0d454af2aeab09933432798a0b563
himesh <himesh@pixelcrayons.com>
2016-04-05 19:07:46 +0530

    added validations for vendor

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,3 +1,4 @@
 class Vendor < ActiveRecord::Base
 	has_many :vendor_products, dependent: :destroy
+	validates :name, :street, :city, :state, :country, :zip, :website, :landline, :email, presence: true
 end
