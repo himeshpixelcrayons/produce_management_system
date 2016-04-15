4ff36714f7d34101f1ddc48227118f80360d2cfb
himesh <himesh@pixelcrayons.com>
2016-04-15 14:26:56 +0530

    Disabled order list on customer change - invoice and delivery

--- app/assets/javascripts/orders.js
+++ app/assets/javascripts/orders.js
@@ -1,13 +1,16 @@
 $(document).ready(function(){
+	$(".order_id").prop( "disabled", true );
 	$(".customer_id").change(function(){
 		var customerId = $(this).val();
 		if(customerId == ""){
 			$(".customer_address").val("");
 			$(".customer_contact").val("");
-			$("#delivery_order_id").empty();
+			$(".order_id").empty();
+			$(".order_id").prop( "disabled", true );
 			alert("Please Select Customer");
 		}
 		else{
+			$(".order_id").prop( "disabled", false );
 			$.ajax({
 				type: "GET",
 				url: '/customers/' + customerId + '/customer_info'
